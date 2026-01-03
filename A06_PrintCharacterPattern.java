2a936629357dbf3313afedbf87b0627c495e3b9d
DanisheKhan <khandanishe125@gmail.com>
2026-01-04 00:53:46 +0530

    Implement half pyramid pattern printing and add character pattern printing class

---
+++ A06_PrintCharacterPattern.java
@@ -0,0 +1,13 @@
+public class A06_PrintCharacterPattern {
+  public static void main(String[] args) {
+    int n = 4;
+    char ch = 'A';
+    for (int lines = 1; lines <= n; lines++) {
+      for (int character = 1; character <= lines; character++) {
+        System.out.print(ch);
+        ch++;
+      }
+      System.out.println();
+    }
+  }
+}
